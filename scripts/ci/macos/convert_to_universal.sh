6c6c0076143eff442a6816124beeee15a7a40d8b
Artur Wrona <arturwrona91@gmail.com>
2022-12-29 16:45:03 +0100

    Fix bugs

--- scripts/ci/macos/convert_to_universal.sh
+++ scripts/ci/macos/convert_to_universal.sh
@@ -4,37 +4,44 @@ PREFIX=${1}
 echo -e "${0} was runned on ${UNAME}\n\n"
 if [ "$(uname)" = "Darwin" ]; then
 echo "Cache location $(brew --cache)"
+LIPO_PREFIX=""
 ls $PREFIX
 else
+LIPO_PREFIX="x86_64-apple-darwin-"
 echo "This system is not MacOS"
 fi
 ####
 makeUniversal() {    
 mimetype=$(file -b --mime-type $1)
-noPrefixPath="${1#arm64/}"
-#echo -e "==> Copying file ${1}\n   as ${mimetype}"
+noPrefixPath="${1#${PREFIX}/arm64}"
+echo -e "==> Processing ${1}\n   as ${mimetype}"
 if [ "${mimetype}" = "application/x-mach-binary" ]; then
-    lipo "x86_64/${noPrefixPath}" "arm64/${noPrefixPath}" -create -output "universal/${noPrefixPath}"
+    ${LIPO_PREFIX}lipo "${PREFIX}/arm64/${noPrefixPath}" "${PREFIX}/x86_64/${noPrefixPath}" -create -output "${PREFIX}/universal/${noPrefixPath}"
     echo "==> Mach-O binary merged: ${noPrefixPath}"
+elif [ "$(file -b ${i})" == "current ar archive" ]; then
+    ${LIPO_PREFIX}lipo "${PREFIX}/arm64/${noPrefixPath}" "${PREFIX}/x86_64/${noPrefixPath}" -create -output "${PREFIX}/universal/${noPrefixPath}"
+    echo "==> AR Archive merged: ${noPrefixPath}"
 else
-    cp "arm64/${noPrefixPath}" "universal/${noPrefixPath}"
+    cp "${PREFIX}/arm64/${noPrefixPath}" "${PREFIX}/universal/${noPrefixPath}"
 fi
 }
+
 ####
 
-FILES=$(find arm64)
+FILES=$(find ${PREFIX}/arm64 -type f)
+DIRS=$(find ${PREFIX}/arm64 -type d)
 
 echo "Creating directory structure"
-for i in $FILES
+for i in $DIRS
 do
-mimetype=$(file -b --mime-type $1)
-noPrefixPath="${1#arm64/}"
-if [ "${mimetype}" = "inode/directory" ]; then
-    mkdir -vp "universal/${noPrefixPath}"
-fi
+universal_dir=$(echo "${i}" | sed "s/\/arm64/universal/g")
+#echo "DIR: ${i} -> ${universal_dir}"
+mkdir -vp "${universal_dir}"
 done
 
 for i in $FILES
 do 
+#echo -e "${i}\n"
 makeUniversal $i
 done
+
