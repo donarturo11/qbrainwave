dc26e6369b06a012ea1b5b319ae4158dd0c96631
Artur Wrona <arturwrona91@gmail.com>
2022-12-29 16:44:31 +0100

    Create script to fetch dependencies

---
+++ scripts/ci/macos/fetch_deps.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+if [ "$(uname)" = "Darwin" ]; then
+echo "Cache location $(brew --cache)"
+BREW_BIN_PATH=""
+ls $PREFIX
+else
+BREW_BIN_PATH="/home/linuxbrew/.linuxbrew/bin/"
+echo "This system is not MacOS"
+fi
+
+mkdir -p ~/brew-cache/{arm64,x86_64}
+mkdir -p ~/brew-mod/{arm64,x86_64}
+
+HOMEBREW_CACHE=~/brew-cache/arm64 ${BREW_BIN_PATH}brew fetch -q --force --bottle-tag=arm64_monterey --deps qt@6
+HOMEBREW_CACHE=~/brew-cache/x86_64 ${BREW_BIN_PATH}brew fetch -q --force --bottle-tag=monterey --deps qt@6
+
+for i in ~/brew-cache/arm64/downloads/*
+do 
+  echo "Preparing ${i} for ARM"
+  tar -xzf "$i" -C ~/brew-mod/arm64
+  rm "$i"
+done
+for i in ~/brew-cache/x86_64/downloads/*
+do 
+  echo "Preparing ${i} for x86_64"
+  tar -xzf "$i" -C ~/brew-mod/x86_64
+  rm "$i"
+done
+
+
+
