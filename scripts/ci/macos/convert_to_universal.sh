f72e01143488769721bd99092c1e04e29f802b41
Artur Wrona <arturwrona91@gmail.com>
2022-12-29 10:04:07 +0100

    Improving finding files

--- scripts/ci/macos/convert_to_universal.sh
+++ scripts/ci/macos/convert_to_universal.sh
@@ -6,19 +6,16 @@ if [ "$(uname)" = "Darwin" ]; then
 echo "Cache location $(brew --cache)"
 ls $PREFIX
 else
-echo "This sistem is not MacOS"
+echo "This system is not MacOS"
 fi
 ####
 makeUniversal() {    
 mimetype=$(file -b --mime-type $1)
 noPrefixPath="${1#arm64/}"
-#echo -e "==> Copyoing file ${1}\n   as ${mimetype}"
+#echo -e "==> Copying file ${1}\n   as ${mimetype}"
 if [ "${mimetype}" = "application/x-mach-binary" ]; then
     lipo "x86_64/${noPrefixPath}" "arm64/${noPrefixPath}" -create -output "universal/${noPrefixPath}"
     echo "==> Mach-O binary merged: ${noPrefixPath}"
-elif [ "${mimetype}" = "inode/directory" ]; then
-    mkdir -vp "universal/${noPrefixPath}"
-    # echo "--- directory copied ${noPrefixPath}"
 else
     cp "arm64/${noPrefixPath}" "universal/${noPrefixPath}"
 fi
@@ -26,6 +23,15 @@ fi
 ####
 
 FILES=$(find arm64)
+
+echo "Creating directory structure"
+for i in $FILES
+do
+mimetype=$(file -b --mime-type $1)
+noPrefixPath="${1#arm64/}"
+if [ "${mimetype}" = "inode/directory" ]; then
+    mkdir -vp "universal/${noPrefixPath}"
+done
 
 for i in $FILES
 do 
